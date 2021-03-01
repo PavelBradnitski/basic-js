57a4ea043c68ecb27030b1cb95e195cc9f11fc53
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2021-03-01 22:18:29 +0300

    Init: start basic-js task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix ) {
+  let count = 0;
+  for(let i = 0; i< matrix.length; i++){
+      for(let y = 0;y< matrix[i].length;y++){
+        if (matrix[i][y] == '^^')  count +=1;
+      }
+  }
+  return count;    
   // remove line with error and write your code here
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  //class DepthCalculator {
+    
+    
+   calculateDepth(arr) {
+    let class1 = new DepthCalculator();
+    return 1 + (arr instanceof Array ? arr.reduce(function(max, item) {
+      return Math.max(max, class1.calculateDepth(item));
+    }, 0) : -1);
+   }
 };
+//Array.prototype.max = function() {
+//  return Math.max.apply(null, this);
+//};
+//let smt = new DepthCalculator();
+
+//console.log(smt.calculateDepth([[[[[]]]]]))
