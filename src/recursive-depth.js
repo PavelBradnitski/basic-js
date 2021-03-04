d60f55f0a92666bc1a890993d71092d004a4e1fd
PavelBradnitski <pavel.brodnizkiy@mail.ru>
2021-03-04 21:07:12 +0300

    fix: complete transform-array task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,16 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  //class DepthCalculator {
-    
-    
-   calculateDepth(arr) {
+  calculateDepth(arr) {
     let class1 = new DepthCalculator();
     return 1 + (arr instanceof Array ? arr.reduce(function(max, item) {
-      return Math.max(max, class1.calculateDepth(item));
-    }, 0) : -1);
-   }
-};
+     return Math.max(max, class1.calculateDepth(item));
+     }, 0) : -1);
+    
+}
+    };      
+  //let smt = new DepthCalculator();
+
+  //console.log(smt.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]));
+
+    
+   //calculateDepth(arr) {
+   // let class1 = new DepthCalculator();
+   // return 1 + (arr instanceof Array ? arr.reduce(function(max, item) {
+   //   return Math.max(max, class1.calculateDepth(item));
+   // }, 0) : -1);
+
 //Array.prototype.max = function() {
 //  return Math.max.apply(null, this);
 //};
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( arr ) {
+  //function transform( arr ) {
+  //throw new CustomError('Not implemented');
+  let arr1 = [];
+  loop1:
+  for(let i = 0;i<arr.length;i++) {
+    switch (arr[i]) {
+      default:
+        arr1.push(arr[i]);
+        break;
+      case '--discard-next':
+        if (i == arr.length - 1) break loop1;
+        //arr.splice(i+1, 1);
+        i++;
+        break;
+      case '--discard-prev':
+        if (i == 0 || arr[i-2] == '--discard-next') continue loop1;
+        arr1.pop();
+        break;
+      case '--double-next':
+        if (i == arr.length - 1 ) break loop1;
+        arr1.push(arr[i+1]);
+        break;
+      case '--double-prev':
+        if (i == 0 || arr[i-2] == '--discard-next') continue loop1;
+        arr1.push(arr[i-1]);
+        break;
+      
+    }
+  }
+  return arr1;
   // remove line with error and write your code here
 };
+//console.log(transform( [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]));
